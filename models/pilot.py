6519208ee7c5d6c01d29d0272ddede8882630d9e
VougneFroid <142602426+VougneFroid@users.noreply.github.com>
2025-12-14 11:29:39 +0800

    added pilot model with CRUD

---
+++ models/pilot.py
@@ -0,0 +1,90 @@
+def get_all(mysql):
+
+    # Get all pilots from the database.
+
+    cursor = mysql.connection.cursor()
+    cursor.execute('''
+        SELECT id, name, flight_years, rank, mission_success 
+        FROM pilot
+        ORDER BY id
+    ''')
+    pilots = cursor.fetchall()
+    cursor.close()
+    return pilots
+
+
+def get_by_id(mysql, pilot_id):
+
+    # Get a specific pilot by ID.
+
+    cursor = mysql.connection.cursor()
+    cursor.execute('''
+        SELECT id, name, flight_years, rank, mission_success 
+        FROM pilot 
+        WHERE id = %s
+    ''', (pilot_id,))
+    pilot = cursor.fetchone()
+    cursor.close()
+    return pilot
+
+
+def create(mysql, data):
+
+    # Create a new pilot.
+
+    cursor = mysql.connection.cursor()
+    cursor.execute('''
+        INSERT INTO pilot (name, flight_years, rank, mission_success)
+        VALUES (%s, %s, %s, %s)
+    ''', (data['name'], data['flight_years'], data['rank'], data['mission_success']))
+    mysql.connection.commit()
+    pilot_id = cursor.lastrowid
+    cursor.close()
+    return pilot_id
+
+
+def update(mysql, pilot_id, data):
+
+    # Build dynamic UPDATE query based on provided fields
+    update_fields = []
+    values = []
+    
+    if 'name' in data:
+        update_fields.append('name = %s')
+        values.append(data['name'])
+    
+    if 'flight_years' in data:
+        update_fields.append('flight_years = %s')
+        values.append(data['flight_years'])
+    
+    if 'rank' in data:
+        update_fields.append('rank = %s')
+        values.append(data['rank'])
+    
+    if 'mission_success' in data:
+        update_fields.append('mission_success = %s')
+        values.append(data['mission_success'])
+    
+    # Add pilot_id to values for WHERE clause
+    values.append(pilot_id)
+    
+    # Execute update query
+    cursor = mysql.connection.cursor()
+    query = f"UPDATE pilot SET {', '.join(update_fields)} WHERE id = %s"
+    cursor.execute(query, values)
+    mysql.connection.commit()
+    rows_affected = cursor.rowcount
+    cursor.close()
+    return rows_affected
+
+
+def delete(mysql, pilot_id):
+
+    # Delete a pilot.
+    
+    cursor = mysql.connection.cursor()
+    cursor.execute('DELETE FROM pilot WHERE id = %s', (pilot_id,))
+    mysql.connection.commit()
+    rows_affected = cursor.rowcount
+    cursor.close()
+    return rows_affected
